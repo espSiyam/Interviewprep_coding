76dcc28a10de132723e3fc13749228bba9fc3033
Siyam <42336409+espSiyam@users.noreply.github.com>
2021-06-07 02:57:53 +0600

    bubble

--- Sorting/2Bubble.cpp
+++ Sorting/2Bubble.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 using namespace std;
-
 void bubbleSort(int arr[], int n)
 {
     int i,j;
     for (i=0; i<n-1;i++)
     {
+        int flag = 0;
         for (j=0;j<n-i-1;j++)
             if (arr[j]>arr[j+1])
-        {
-            int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-        }
-
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = 1;
+                }
+        if(flag == 0)
+            break;
     }
-
 }
 
 int main()
 {
-    int data[] = {-2,45,0,11,-9};
+    int data[] = {5,4,3,2,1};
     int size = sizeof(data)/sizeof(data[0]);
     bubbleSort(data, size);
     cout<< "Sorted Array: \n";
